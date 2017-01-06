5685411cddcfa48f6a340cd4e41c5277aec28457
FREESKY <904834133@qq.com>
2017-01-06 16:00:13 +0800

    Initial commit

--- syxownlib/src/main/java/com/syx/syxownlib/LogUtils.java
+++ syxownlib/src/main/java/com/syx/syxownlib/LogUtils.java
@@ -12,31 +12,31 @@ public class LogUtils {
         throw new UnsupportedOperationException("u can't instantiate me...");
     }
 
-    public void LogD(boolean flag,String TAG,String message){
+    public static void LogD(boolean flag,String TAG,String message){
         if (flag==true)
         {
             Log.d(TAG,message);
         }
     }
-    public void LogE(boolean flag,String TAG,String message){
+    public static void LogE(boolean flag,String TAG,String message){
         if (flag==true)
         {
             Log.e(TAG,message);
         }
     }
-    public void LogW(boolean flag,String TAG,String message){
+    public static void LogW(boolean flag,String TAG,String message){
         if (flag==true)
         {
             Log.w(TAG,message);
         }
     }
-    public void LogI(boolean flag,String TAG,String message){
+    public static void LogI(boolean flag,String TAG,String message){
         if (flag==true)
         {
             Log.i(TAG,message);
         }
     }
-    public void LogV(boolean flag,String TAG,String message){
+    public static void LogV(boolean flag,String TAG,String message){
         if (flag==true)
         {
             Log.v(TAG,message);
